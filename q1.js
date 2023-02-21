e2c811d1697f68373927f96296c79db7c1d43b8b
FasihUddin <fasihuddin002@gmail.com>
2023-02-21 16:15:24 +0500

    Question_2 done

--- q1.js
+++ q1.js
@@ -1,3 +1,4 @@
 //Question-1
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 var personName = "Fasih";
 console.log("Hello ".concat(personName, " Eric, would you like to learn some Python today?"));
---
+++ q2.js
@@ -0,0 +1,14 @@
+//Question-2
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+//let personName = "faSih udDIn";
+//console.log(personName.toLowerCase()); //LowerCase method
+//console.log(personName.toUpperCase()); //UpperCase method
+//By using for loop to titleCase a string
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+console.log(titleCase("huSSAin aLi"));
