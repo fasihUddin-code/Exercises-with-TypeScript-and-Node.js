4fc311232bc4187958254c89c69efa2f89ec124d
FasihUddin <fasihuddin002@gmail.com>
2023-02-21 13:11:00 +0500

    Question_1_done

---
+++ q1.js
@@ -0,0 +1,3 @@
+//Question-1
+var personName = "Fasih";
+console.log("Hello ".concat(personName, " Eric, would you like to learn some Python today?"));
---
+++ q1.ts
@@ -0,0 +1,3 @@
+//Question-1
+let personName:string = "Fasih";
+console.log(`Hello ${personName} Eric, would you like to learn some Python today?`);
